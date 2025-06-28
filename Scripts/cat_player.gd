cc8112196fe44dc8d06c1aecd3436d55391c7ff1
IsabeleSilvestre <isabele.silvestre.27@gmail.com>
2025-06-28 07:15:11 -0300

    att

--- Scripts/cat_player.gd
+++ Scripts/cat_player.gd
@@ -1,45 +1,59 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
-@export var jump_force: float = 400.0
-@export var gravity: float = 900.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+const GRAVITY = 1000
+const SPEED = 300
+const JUMP = -350
 
-func _ready() -> void:
-	pass
+enum State {Idle, Run, Jump}
 
-func _physics_process(delta: float) -> void:
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		if velocity.y > 800:
-			velocity.y = 800
-	else:
-		if not Input.is_action_just_pressed("ui_up"):
-			velocity.y = 0.0
+var current_state
 
-	var input_direction: float = Input.get_axis("ui_left", "ui_right")
-	velocity.x = input_direction * speed
+func _ready():
+	current_state = State.Idle
 
-	if input_direction != 0:
-		if input_direction < 0:
-			animated_sprite.flip_h = true
-		else:
-			animated_sprite.flip_h = false
-
-	if Input.is_action_just_pressed("ui_up") and is_on_floor:
-		velocity.y = -jump_force
-
+func _physics_process(delta):
+	palyer_falling(delta)
+	player_idle(delta)
+	player_run(delta)
+	player_jump(delta)
+	
 	move_and_slide()
+	
+	player_animations()
 
-	if animated_sprite:
-		if is_on_floor:
-			if input_direction != 0:
-				animated_sprite.play("run")
-			else:
-				animated_sprite.play("idle")
-		else:
-			if velocity.y < 0:
-				animated_sprite.play("jump")
-			else:
-				animated_sprite.play("fall")
+func player_jump(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = JUMP
+		current_state = State.Jump
+
+func palyer_falling(delta):
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+		
+		
+func player_idle(delta):
+	if is_on_floor():
+		current_state = State.Idle
+
+func player_run(delta):
+	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction != 0 and is_on_floor():
+		current_state = State.Run
+		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+func player_animations():
+	if current_state == State.Idle:
+		animated_sprite_2d.play("Idle")
+	elif current_state == State.Run:
+		animated_sprite_2d.play("Run")
+	elif current_state == State.Jump:
+		animated_sprite_2d.play("Jump")
+	
